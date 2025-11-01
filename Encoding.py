3620bfc775b62cf5e9f6bd5b964945809f6c2bdb
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2025-11-01 14:52:50 +0300

    some changes

--- Encoding.py
+++ Encoding.py
@@ -1,12 +1,9 @@
 import pandas as pd
 
-# 1️⃣ Excel dosyasını oku
 df = pd.read_excel("/Users/yasemin/Desktop/hi1_2025_09_eylul 2.xlsx")
 
-# 2️⃣ Kategorik (harf veya metin içeren) sütunları seç
 categorical_cols = df.select_dtypes(include=["object"]).columns
 
-# 3️⃣ One-Hot Encoding uygula
 df_encoded = pd.get_dummies(df, columns=categorical_cols, drop_first=False)
 
 df_encoded.to_excel("hi1_2025_09_eylul_encoded.xlsx", index=False)
