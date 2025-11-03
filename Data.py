7badff706d8a6b00adf595142f1571f747cb9a8a
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2025-11-03 22:59:51 +0300

    png to pdf changes

--- Data.py
+++ Data.py
@@ -8,20 +8,26 @@ input_path = os.path.join(base_dir, "hi1_2025_09_eylul.xlsx")
 
 df = pd.read_excel(input_path)
 
-
 df['Kayıt Tarihi'] = pd.to_datetime(df['Kayıt Tarihi'])
 
 columns_to_drop = ['Kayıt Yapan', 'CO', 'No2']
 df_cleaned = df.drop(columns=columns_to_drop)
+
 df_cleaned.set_index('Kayıt Tarihi', inplace=True)
 
 df_resampled = df_cleaned.resample('15T').mean(numeric_only=True)
 
-print(" Resampling successful")
+print("Resampling successful")
 
 output_path = os.path.join(base_dir, "hi1_2025_09_eylul_15min.xlsx")
 df_resampled.to_excel(output_path)
 
-df_resampled.plot(figsize=(10,5), title="15-Minute Averaged Values (Resampled Data)")
+plt.figure(figsize=(10, 5))
+df_resampled.plot(ax=plt.gca(), title="15-Minute Averaged Values (Resampled Data)")
 plt.tight_layout()
-plt.show()
+
+pdf_output_path = os.path.join(base_dir, "hi1_2025_09_eylul_15min_plot.pdf")
+plt.savefig(pdf_output_path, format='pdf')
+
+plt.close()
+print(f"PDF created: {pdf_output_path}")
